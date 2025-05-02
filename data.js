e00650bf525ed52086d7a2470b81c83b2fb14ac8
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2025-05-02 01:58:37 +0100

    Update data.js

--- data.js
+++ data.js
@@ -10,37 +10,6 @@ export const messageData = JSON.parse(localStorage.getItem("nacos")) || [
 
 export const computerResponseData = [
   {
-    key: ["help", "assist", "confused"],
-    message: "What can i help you with?",
-  },
-  {
-    key: ["fuck", "love", "sex", "knack"],
-    message: "I don't have an experience with that yet ",
-  },
-  {
-    key: ["slaughter", "murder", "kill", "steal", "kidnap", "rape"],
-    message: "I can't help you with that",
-  },
-  {
-    key: ["don't"],
-    message:
-      "Why? Eze Daniel has shown his experience in helping NACOS in different fields e.g: QA engineer",
-  },
-  {
-    key: ["truthful", "accurate", "answer"],
-    message: "I will give my best response to your question. What is it.?",
-  },
-  {
-    key: ["skills", "experience", "why","reason","reasons"],
-    message:
-      "<img style='object-position: right;' src='img/eze 2.jpg' alt='Eze Skills' loading='lazy' />",
-  },
-  {
-    key: ["vote", "president"],
-    message:
-      "<img src='img/eze.jpg' alt='Vote flier for eze' loading='lazy' />",
-  },
-  {
     key: [
       "hello",
       "hi",
@@ -54,5 +23,36 @@ export const computerResponseData = [
       "gee",
     ],
     message: "I am alright, how you doing today ",
+  },
+  {
+    key: ["help", "assist", "confused"],
+    message: "What can i help you with?",
+  },
+  {
+    key: ["fuck", "love", "sex", "knack"],
+    message: "I don't have an experience with that yet ",
+  },
+  {
+    key: ["slaughter", "murder", "kill", "steal", "kidnap", "rape"],
+    message: "I can't help you with that",
+  },
+  {
+    key: ["vote", "president"],
+    message:
+      "<img src='img/eze.jpg' alt='Vote flier for eze' loading='lazy' />",
+  },
+  {
+    key: ["don't"],
+    message:
+      "Why? Eze Daniel has shown his experience in helping NACOS in different fields e.g: QA engineer",
+  },
+  {
+    key: ["truthful", "accurate", "answer"],
+    message: "I will give my best response to your question. What is it.?",
+  },
+  {
+    key: ["skills", "experience", "why"],
+    message:
+      "<img style='object-position: right;' src='img/eze 2.jpg' alt='Eze Skills' loading='lazy' />",
   },
 ];
